b94a22add328c093e89bba7ad2a603b774ee8652
maccali <guilherme.maccali@universo.univates.br>
2021-04-18 16:26:46 -0300

    fix: test migrations

---
+++ prisma/migrations/20210418192419_add_another_avatar/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "avatar2" TEXT NOT NULL DEFAULT E'';
---
+++ prisma/migrations/20210418192546_add_other_avatar/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "avatar3" TEXT NOT NULL DEFAULT E'';
